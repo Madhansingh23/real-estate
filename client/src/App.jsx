022e370015a30b902ed3d92aec1091ae5c5c9812
Madhansingh23 <t.madhansingh23@gmail.com>
2025-02-19 22:49:48 +0530

    created files in client with pages and compoennets for further improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import './index.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import Contact from './pages/Contact'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="underline text-bold">HI </h1>
-    </div>
+    <>
+    <BrowserRouter>
+
+    <Header/>
+
+    <Routes>
+
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signout" element={<SignOut/>}/>
+      
+    </Routes>
+    <Footer/>
+
+    </BrowserRouter>
+    </>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='bg-slate-200 shadow-md'>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className='font-bold text-sm sm:text-xl  flex flex-wrap'>
+          <span className="text-slate-500">Group</span>
+          <span className='text-slate-700'>Estate</span>
+          </h1>
+          <form action="" className='bg-slate-100'>
+            <input type="text" placeholder="Search..." className='bg-transparent'  />
+          </form>
+      </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
