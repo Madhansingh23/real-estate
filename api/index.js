ca4d0a4e11f7f24165b0f191f19f4d056fb0d352
Madhansingh23 <t.madhansingh23@gmail.com>
2025-02-19 23:59:39 +0530

    connecting to mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoos from 'mongoose'
 
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDb bro!");
+}).catch((err)=>{
+    console.log(err);
+})
 const app=express();
 
 app.listen(3000,()=>{
